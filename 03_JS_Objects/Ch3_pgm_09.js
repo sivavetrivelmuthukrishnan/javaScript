23eb185d11c1127705e513b9cbd933472afa9ce1
SIVAVETRIVEL <sivavetrivel07502@gmail.com>
2024-07-10 15:46:40 +0530

    3-9 completed

--- 03_JS_Objects/Ch3_pgm_09.js
+++ 03_JS_Objects/Ch3_pgm_09.js
@@ -2,19 +2,29 @@
 
 var book1;
 var book2;
+var book3;
 
 book1 = {
 	title: "The Hobbit",
-	author: "J. R. R. Tolkien"
+	author: "J. R. R. Tolkien",
+	published : 2002
 };
 
 book2 = {
 	title: "Northern Lights",
-	author: "Philip Pullman"
+	author: "Philip Pullman",
+	published : 2000
 };
 
-console.log(book1.title + " by " + book1.author);
-console.log(book2.title + " by " + book2.author);
+book3 = {
+	title : "Wings Of Fire",
+	author : "Dr.A.P.J.Abdul Kalam",
+	published : 1999
+}
+
+console.log(book1.title + " by " + book1.author + "published in " + book1.published);
+console.log(book2.title + " by " + book2.author + "published in " + book2.published);
+console.log(book2.title + " by " + book2.author + "published in " + book2.published);
 
 
 
