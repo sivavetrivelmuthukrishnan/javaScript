af7a976aa64818c557e3e5baea85b26420a010ef
SIVAVETRIVEL <sivavetrivel07502@gmail.com>
2024-07-10 14:30:47 +0530

    2.6 completed

--- 02_JS_variables/Ch2_pgm_06.js
+++ 02_JS_variables/Ch2_pgm_06.js
@@ -5,8 +5,10 @@ var locationName;
 
 playerName = "Lionel Messi";
 locationName = "Argentina";
+score = 100;
 
 console.log(playerName + " is in " + locationName);
+console.log(playerName + " has a score of " +score);
 
 
 
