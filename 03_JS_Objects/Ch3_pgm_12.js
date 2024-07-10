ccff44093bf46a8571a9e18792d71f3e6ce9b761
SIVAVETRIVEL <sivavetrivel07502@gmail.com>
2024-07-10 21:10:01 +0530

    3-12 completed

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -7,8 +7,24 @@ var post = {
 	created : "2015-06-21",
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
+console.log("the post id is " + post.id);
+console.log("The post title is " + post.title);
+console.log("The post author is " + post.author);
+ 
 
+var post2 = {
+	id : 2,
+	title : "WIngs Of Fire",
+	author : "Dr.A.P.J.Abdul Kalam",
+	created : 1999,
+	body : "Motivated to youth"
+}
 
+console.log("The post2 id is " + post2.id)
+console.log("The post2 title is " + post2.title)
+console.log("The post2 author is " + post2.author)
+console.log("The post2 created is " + post2.created)
+console.log("The post2 body is " + post2.body) 
 
 /* Further Adventures
  *
