d9bc9c12d30a2bf25441e1808b3859bd2caa5035
SIVAVETRIVEL <sivavetrivel07502@gmail.com>
2024-07-10 14:32:52 +0530

    2.8 completed

--- 02_JS_variables/Ch2_pgm_08.js
+++ 02_JS_variables/Ch2_pgm_08.js
@@ -1,14 +1,17 @@
 // Using a variable’s current value to set its new value
-
 var score;
 score = 100;
 console.log("Your score was " + score);
 
 console.log("Great splat!!!");
 score = score + 50;
-console.log("New score: " + score);
 
+console.log("New score: " + score);
 console.log("Way to go!");
+
+
+score = score+100;
+console.log("Congratulation ! you are great " + score);
 
 
 
