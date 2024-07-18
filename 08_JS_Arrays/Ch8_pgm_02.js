29ca3256940f607152e04cd1e2168febe5cedac0
SIVAVETRIVEL <sivavetrivel07502@gmail.com>
2024-07-18 17:55:35 +0530

    8-2 completed

--- 08_JS_Arrays/Ch8_pgm_02.js
+++ 08_JS_Arrays/Ch8_pgm_02.js
@@ -7,6 +7,8 @@ var place3 = { name : "Bondi Beach", country : "Australia" };
 var thisYear = [ place1, place2 ];
 var nextYear = [ place3 ];
 
+nextYear.push(place1)
+
 console.log(thisYear);
 console.log(nextYear);
 
